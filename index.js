bf3cb0c79a17ba56c9f8529a02dbbb839c539459
Cami Blanch <caminblanch@gmail.com>
2019-03-28 17:22:52 -0600

    Stubbed out index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	isValid: require("./src/isInvalid"),
+	isInvalid: require("./src/isInvalid"),
+	isAmbiguous: require("./src/isInvalid"),
+	isMissingSecondary: require("./src/isInvalid")
+};
