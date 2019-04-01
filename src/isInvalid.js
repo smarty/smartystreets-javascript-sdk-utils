9316806f0f3610f5879cb7f17940809c1c1b28a4
Cami Blanch <caminblanch@gmail.com>
2019-04-01 11:16:20 -0600

    Refactored and simplified if else logic

--- src/isInvalid.js
+++ src/isInvalid.js
@@ -1,20 +1,18 @@
 function isInvalid (result) {
 	const noResults = result.length === 0;
-	const exactlyOneResult = result.length === 1;
 
 	if (noResults) {
 		return true;
-	} else if (exactlyOneResult) {
-		const addressPrecisionIsPreciseEnough = result[0].analysis.addressPrecision && result[0].analysis.addressPrecision !== "Premise" && result[0].analysis.addressPrecision !== "DeliveryPoint";
-		const verificationStatusIsNone = result[0].analysis.verificationStatus === "None";
-		const dpvMatchCodeIsN = result[0].analysis.dpvMatchCode === "N";
-		const undefinedDpvMatchCodeAndVerificationStatus = result[0].analysis.verificationStatus === undefined && result[0].analysis.dpvMatchCode === undefined;
-
-		if (verificationStatusIsNone || (addressPrecisionIsPreciseEnough || dpvMatchCodeIsN || undefinedDpvMatchCodeAndVerificationStatus)) {
-			return true;
-		}
 	}
-	return false;
+
+	const exactlyOneResult = result.length === 1;
+	const addressPrecisionIsPreciseEnough = result[0].analysis.addressPrecision && result[0].analysis.addressPrecision !== "Premise" && result[0].analysis.addressPrecision !== "DeliveryPoint";
+	const verificationStatusIsNone = result[0].analysis.verificationStatus === "None";
+	const dpvMatchCodeIsN = result[0].analysis.dpvMatchCode === "N";
+	const undefinedDpvMatchCodeAndVerificationStatus = result[0].analysis.verificationStatus === undefined && result[0].analysis.dpvMatchCode === undefined;
+
+	return !!(exactlyOneResult && (verificationStatusIsNone || (addressPrecisionIsPreciseEnough || dpvMatchCodeIsN || undefinedDpvMatchCodeAndVerificationStatus)));
+
 }
 
 module.exports = isInvalid;
