bae532e92573ca931a654c192e0aba8a267808bd
Cami Blanch <caminblanch@gmail.com>
2019-04-01 10:56:01 -0600

    Refactored out variables from isInvalid

--- src/isInvalid.js
+++ src/isInvalid.js
@@ -1,8 +1,14 @@
 function isInvalid (result) {
-	if (result.length === 0) {
+	const noResults = result.length === 0;
+	const exactlyOneResult = result.length === 1;
+
+	if (noResults) {
 		return true;
-	} else if (result.length === 1 && result[0].analysis.verificationStatus === "None") {
-		return true;
+	} else if (exactlyOneResult) {
+		const verificationStatus = result[0].analysis.verificationStatus;
+		if (verificationStatus === "None") {
+			return true;
+		}
 	}
 	return false;
 }
