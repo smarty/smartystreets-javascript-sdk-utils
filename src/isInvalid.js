54f0953c110d4ec1afd8076bba4042084a96d438
Cami Blanch <caminblanch@gmail.com>
2019-04-01 10:42:01 -0600

    Invalid address has length 1 and is has verification status of none

--- src/isInvalid.js
+++ src/isInvalid.js
@@ -1,6 +1,8 @@
 function isInvalid (result) {
 	if (result.length === 0) {
 		return true;
+	} else if (result.length === 1 && result[0].analysis.verificationStatus === "None") {
+		return true;
 	}
 	return false;
 }
--- tests/test_isInvalid.js
+++ tests/test_isInvalid.js
@@ -4,8 +4,18 @@ const isInvalid = require("../src/isInvalid");
 
 describe("An invalid address", function () {
 	it("is an empty result.", function () {
-		const verifiedAddress = [];
+		const invalidAddress = [];
 
-		expect(isInvalid(verifiedAddress)).to.equal(true);
+		expect(isInvalid(invalidAddress)).to.equal(true);
+	});
+
+	it("is exactly one item and has a verification status of 'None'", function () {
+		const invalidAddress = [{
+			analysis: {
+				verificationStatus: "None",
+			},
+		}];
+
+		expect(isInvalid(invalidAddress)).to.equal(true);
 	});
 });
